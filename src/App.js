8c36ed3eb973d2c35f61cc93bbd4e9bd195fceb9
mitulpatel224 <mitul.patel224@gmail.com>
2021-08-08 22:16:18 +0530

    feat: monster rolodex

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import { Component } from 'react';
-import FetchUser from './components/fetch-user'
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { monsters: [], search: '' }
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(monsters => this.setState({ monsters }))
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ search: target.value })
+  }
+
   render() {
     return (
       <div className="App">
-        <FetchUser></FetchUser>
-      </div>
+        {/* <FetchUser></FetchUser> */}
+        <h1>Monster Rolodex</h1>
+        <SearchBox placeholder="Search Monsters" handleChange={this.handleChange}></SearchBox>
+        <CardList monsters={this.state.monsters} search={this.state.search}></CardList>
+      </div >
     );
   }
 }
